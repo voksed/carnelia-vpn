264a9dcdd7665c2e35bca50bcd1303aa07b918ab
Carnelia Dev <dev@carnelia.vpn>
2026-02-01 02:31:04 +0500

    Fix incorrect plugin versions and usage in app module

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.2.0"
-    kotlin("android") version "1.9.20"
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
